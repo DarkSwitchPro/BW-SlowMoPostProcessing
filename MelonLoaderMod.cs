b7c2030648269ae998b130ff6cdd5ae43ed9868a
DarkSwitchPro <59634615+DarkSwitchPro@users.noreply.github.com>
2021-12-10 23:52:45 -0500

    BW-SlowMoPostProcessing

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using StressLevelZero.Rig;
+
+namespace SlowMoPostProcessing
+{
+    public static class BuildInfo
+    {
+        public const string Name = "SlowMoPostProcessing";
+        public const string Author = "DarkSwitchPro";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class SlowMoPostProcessing : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Time.timeScale <= 0.0f)
+                return;
+
+            PostProcessVolume v = GameObject.Find("[RigManager (Default Brett)]").GetComponent<RigManager>().gameWorldSkeletonRig.m_head.GetChild(0).GetComponent<PostProcessVolume>();
+            ChromaticAberration ab = v.profile.settings[3].Cast<ChromaticAberration>();
+            ab.intensity.value = 0.1f / Time.timeScale;
+            v.profile.settings[3] = ab;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(SlowMoPostProcessing.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(SlowMoPostProcessing.BuildInfo.Company)]
+[assembly: AssemblyProduct(SlowMoPostProcessing.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + SlowMoPostProcessing.BuildInfo.Author)]
+[assembly: AssemblyTrademark(SlowMoPostProcessing.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(SlowMoPostProcessing.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(SlowMoPostProcessing.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(SlowMoPostProcessing.SlowMoPostProcessing), SlowMoPostProcessing.BuildInfo.Name, SlowMoPostProcessing.BuildInfo.Version, SlowMoPostProcessing.BuildInfo.Author, SlowMoPostProcessing.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
